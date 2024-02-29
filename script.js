50ebc3d7cbfbacd9cbd71290cbf7907981f46918
sigmoza <113186270+Bartek163@users.noreply.github.com>
2024-02-29 19:46:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,7 @@
  function z40() {
     window.open("z40_Mur.html", "okienko_z40","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
  }
+
+ function z38w() {
+    window.open("z38w_Mur.html", "okienko_z38","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
