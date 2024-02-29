8f9430dca52c812c5315369a69e76a349500076e
sigmoza <113186270+Bartek163@users.noreply.github.com>
2024-02-29 19:32:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-function z35() {
+ function z35() {
     window.open("z35_Mur.html", "okienko_z35","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
  }
  
  function z36() {
     window.open("z36_Mur.html", "okienko_z36","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
  }
- 
- function z37() {
-    window.open("z37_Mur.html", "okienko_z37","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
- }
- 
+
  function z38() {
     window.open("z38_Mur.html", "okienko_z38","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
  }
