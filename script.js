00c6e1140bd420c43cf6bdab2a33c3d850804f34
sigmoza <113186270+Bartek163@users.noreply.github.com>
2024-02-29 19:24:23 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function z35() {
+    window.open("z35_Mur.html", "okienko_z35","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+ 
+ function z36() {
+    window.open("z236_Mur.html", "okienko_z36","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+ 
+ function z37() {
+    window.open("z37_Mur.html", "okienko_z37","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ 
+ function z38() {
+    window.open("z38_Mur.html", "okienko_z38","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ 
+ function z39() {
+    window.open("z39_Mur.html", "okienko_z39","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+
+ function z40() {
+    window.open("z40_Mur.html", "okienko_z40","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
